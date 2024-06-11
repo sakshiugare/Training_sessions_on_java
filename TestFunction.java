a517d7bd0e200c723b8fd26de8ae540aa79a6910
sakshiugare <142111511+sakshiugare@users.noreply.github.com>
2024-06-11 15:06:34 +0530

    Add files via upload

---
+++ TestFunction.java
@@ -0,0 +1,15 @@
+package sak.com;
+
+
+public class TestFunction {
+
+	public static void main(String[] args) {
+		
+		//funcDemo fd=new funcDemo();
+		funcDemo.add();
+		funcDemo.sub(12, 10);
+
+
+	}
+
+}
---
+++ funcDemo.java
@@ -0,0 +1,16 @@
+package sak.com;
+
+public class funcDemo {
+	public static void add() {
+		int a=10;
+		int b=20;
+		int res=a+b;
+		System.out.println("Addition is:"+res);
+	}
+	public static void sub(int a,int b) {
+		
+		int res=a-b;
+		System.out.println("Sub is:"+res);
+	}
+	
+}
